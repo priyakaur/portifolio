d1108f45408d390c1e146988dc5a8f9a14c47699
priyakaur <76507891+priyakaur@users.noreply.github.com>
2021-01-10 20:51:56 +0530

    Final without CSV

--- single_parameter_search.py
+++ single_parameter_search.py
@@ -1,35 +1,26 @@
 import csv,sys
-import PySimpleGUI as sg
 from googlesearch import search
+import pandas as pd
 
 End = "Your code has been executed!"
 Er = "Some Error Occurred!"
 
 try:
-# to interact with CSV File.
-    file = open("google_search.csv",'r')
-    write = open("urls_results.csv",'w',newline='')
-    fieldnames = ['Search', 'Result_URL']
-    csvFile = csv.reader(file,delimiter=',')
-    csvWrite = csv.DictWriter(write, fieldnames=fieldnames)
+# to interact with data
+    names = ['Elon Musk', 'Narendar Modi', 'mark zuckerberg', 'Barak Obama', 'Arvind Kejriwal']
     result = []
+    DB = pd.DataFrame(names)
     
 # to search
-    for row in csvFile:
-        query = ",".join(row)
+    for row in names:
+        query = row + ' ' + 'LinkedIn'
         print(query)
         for i in search(query, tld='com', lang='en', num=1, stop=1, pause=0.5):
             print(i)
             result.append(i)
-            csvWrite.writerow({'Search': query, 'Result_URL':i})
 
-# end the code
-    file.close()
-    write.close()
-    
+# end the code    
     print(End)
-    sg.Popup(End)
 
 except:
     print(Er)
-    sg.Popup(Er)
